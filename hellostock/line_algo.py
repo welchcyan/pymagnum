35298030376a9c8be1f948f46a04f7c956d51abc
chenwbj <chenwbj@gmail.com>
2016-07-02 00:24:14 +0800

    从csv文件读取股票daily数据 def read_stock(fileurl):     # type: (str) -> pd.DataFrame

--- hellostock/line_algo.py
+++ hellostock/line_algo.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 def read_idx(fileurl):
+    # type: (str) -> list, ndarray
     f = open(fileurl)
     flist = [[line.split(',')[1], line.split(',')[5]] for line in f]
     flist.reverse()
@@ -12,6 +13,28 @@ def read_idx(fileurl):
     index_val = np.array(index_value[:, 1], dtype=np.float64)
     f.close()
     return x_axis, index_val
+
+
+def read_stock(fileurl):
+    # type: (str) -> pd.DataFrame
+    f = open(fileurl)
+    flist = []
+    f.next()
+    for line in f:
+        line_val = line.split(',')
+        flist.append(
+            [line_val[1], line_val[2], line_val[3], line_val[4], line_val[5], line_val[7]])
+    flist.reverse()
+    flist = np.array(flist)
+    ind = np.array(flist[:, 0], dtype=pd.Timestamp)
+    data = {'date': pd.Series(flist[:, 0], index=ind), 'open': pd.Series(flist[:, 1], index=ind),
+            'high': pd.Series(flist[:, 2], index=ind), 'low': pd.Series(flist[:, 3], index=ind),
+            'close': pd.Series(flist[:, 4], index=ind), 'vol': pd.Series(flist[:, 5], index=ind)
+            }
+    col = ['date', 'open', 'high', 'low', 'close', 'vol']
+    stock_frame = pd.DataFrame(data, index=ind, columns=col)
+    f.close()
+    return stock_frame
 
 
 def moving_average_1(ma_step, sourcelist):

97b1fed2c192d20ce438627d3723f42f6b666b8e
chenwbj <chenwbj@gmail.com>
2016-08-06 00:56:50 +0800

    add batch read stockfile method

--- hellostock/filehandler.py
+++ hellostock/filehandler.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 
@@ -28,11 +29,27 @@ def read_stock(fileurl):
     flist = np.array(flist)
     ind = np.array(flist[:, 0], dtype=pd.Timestamp)
     data = {'date': pd.Series(flist[:, 0], index=ind, dtype=pd.Timestamp), 'symbol': pd.Series(flist[:, 1], index=ind),
-            'open': pd.Series(flist[:, 2], index=ind, dtype=float), 'high': pd.Series(flist[:, 3], index=ind,dtype=float),
-            'low': pd.Series(flist[:, 4], index=ind,dtype=float), 'close': pd.Series(flist[:, 5], index=ind,dtype=float),
-            'vol': pd.Series(flist[:, 6], index=ind,dtype=float)
+            'open': pd.Series(flist[:, 2], index=ind, dtype=float),
+            'high': pd.Series(flist[:, 3], index=ind, dtype=float),
+            'low': pd.Series(flist[:, 4], index=ind, dtype=float),
+            'close': pd.Series(flist[:, 5], index=ind, dtype=float),
+            'vol': pd.Series(flist[:, 6], index=ind, dtype=float)
             }
     col = ['date', 'symbol', 'open', 'high', 'low', 'close', 'vol']
     stock_frame = pd.DataFrame(data, index=ind, columns=col)
     f.close()
     return stock_frame
+
+
+def read_stocks(dir_url):
+    # type: (str) -> dict
+
+    """
+
+    :type dir_url: str
+    """
+    _dfd = dict()
+    for fpathe, dirs, fs in os.walk(dir_url):
+        for f in fs:
+            _dfd[f.split('.')[0]] = read_stock(os.path.join(fpathe, f))
+    return _dfd
